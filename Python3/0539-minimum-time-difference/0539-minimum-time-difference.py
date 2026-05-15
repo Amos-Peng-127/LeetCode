526007afe78282493715a19c5b6de20a49e3f912
Zhixiang Peng <peng0127@bu.edu>
2026-05-15 19:45:14 +0800

    Time: 7 ms (80.12%), Space: 21.1 MB (84.54%) - LeetHub

--- Python3/0539-minimum-time-difference/0539-minimum-time-difference.py
+++ Python3/0539-minimum-time-difference/0539-minimum-time-difference.py
@@ -1,18 +1,50 @@
 class Solution:
     def findMinDifference(self, timePoints: List[str]) -> int:
         
+        t_mins = [False for _ in range(24 * 60)]
+
         def convert(t):
             h, m = t.split(':')
             return int(h) * 60 + int(m)
-        t_mins = list(map(convert, timePoints))
-        t_mins.sort()
+        
+        for t in timePoints:
+            if t_mins[convert(t)]:
+                return 0
+            t_mins[convert(t)] = True
 
-        total_mins = 24 * 60
+        prev_seen = None
+        first_seen = None
+        last_seen = None
+        min_diff = float('inf')
 
-        min_diff = total_mins - t_mins[-1] + t_mins[0]
+        for i in range(1440):
+            if t_mins[i]:
+                last_seen = i
+                if prev_seen == None:
+                    prev_seen = i
+                    first_seen = i
+                    continue
+                min_diff = min(min_diff, i - prev_seen)
+                prev_seen = i
 
-        for i in range(len(t_mins) - 1):
-            clockwise = t_mins[i + 1] - t_mins[i]
-            min_diff = min(min_diff, clockwise)
+        min_diff = min(min_diff, 1440 - (last_seen - first_seen))
 
         return min_diff
+
+
+        # # Time Complexity O(nlog(n)), Space Complexity O(n)
+        # def convert(t):
+        #     h, m = t.split(':')
+        #     return int(h) * 60 + int(m)
+        # t_mins = list(map(convert, timePoints))
+        # t_mins.sort()
+
+        # total_mins = 24 * 60
+
+        # min_diff = total_mins - t_mins[-1] + t_mins[0]
+
+        # for i in range(len(t_mins) - 1):
+        #     clockwise = t_mins[i + 1] - t_mins[i]
+        #     min_diff = min(min_diff, clockwise)
+
+        # return min_diff
