57ed339b6794753a34d72b621d9a4aa13a91bc93
Zhixiang Peng <peng0127@bu.edu>
2026-07-24 06:37:17 +0800

    Time: 27 ms (5.52%), Space: 46.8 MB (9.21%) - LeetHub

---
+++ Java/0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        // Sort
+
+        // char[] charS = s.toCharArray();
+        // char[] charT = t.toCharArray();
+
+        // Arrays.sort(charS);
+        // Arrays.sort(charT);
+
+        // return Arrays.equals(charS, charT);
+
+        // Hash Map
+        HashMap<Character, Integer> countS = new HashMap<>();
+        HashMap<Character, Integer> countT = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            countS.put(s.charAt(i), countS.getOrDefault(s.charAt(i), 0) + 1);
+            countT.put(t.charAt(i), countT.getOrDefault(t.charAt(i), 0) + 1);
+        }
+        
+        return countS.equals(countT);
+    }
+}
