39bf371a13264be740fc2124dc0d2810b1561cc2
Zhixiang Peng <peng0127@bu.edu>
2026-07-24 06:43:00 +0800

    Time: 6 ms (43.23%), Space: 44.2 MB (82.71%) - LeetHub

--- Java/0242-valid-anagram/0242-valid-anagram.java
+++ Java/0242-valid-anagram/0242-valid-anagram.java
@@ -14,14 +14,30 @@ class Solution {
         // return Arrays.equals(charS, charT);
 
         // Hash Map
-        HashMap<Character, Integer> countS = new HashMap<>();
-        HashMap<Character, Integer> countT = new HashMap<>();
+        // HashMap<Character, Integer> countS = new HashMap<>();
+        // HashMap<Character, Integer> countT = new HashMap<>();
+
+        // for (int i = 0; i < s.length(); i++) {
+        //     countS.put(s.charAt(i), countS.getOrDefault(s.charAt(i), 0) + 1);
+        //     countT.put(t.charAt(i), countT.getOrDefault(t.charAt(i), 0) + 1);
+        // }
+        
+        // return countS.equals(countT);
+
+        // Hash Table
+        int[] count = new int[26];
 
         for (int i = 0; i < s.length(); i++) {
-            countS.put(s.charAt(i), countS.getOrDefault(s.charAt(i), 0) + 1);
-            countT.put(t.charAt(i), countT.getOrDefault(t.charAt(i), 0) + 1);
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
         }
-        
-        return countS.equals(countT);
+
+        for (int val : count) {
+            if (val != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
