d0f35d71e6408856e92884e86339c0cc141d8268
Zhixiang Peng <peng0127@bu.edu>
2026-07-25 06:06:23 +0800

    Time: 7 ms (46.47%), Space: 47.4 MB (10.97%) - LeetHub

--- Java/0001-two-sum/0001-two-sum.java
+++ Java/0001-two-sum/0001-two-sum.java
@@ -1,18 +1,54 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        // Sorting
+        int[] origin = nums.clone();
+        Arrays.sort(nums);
         
-        Map<Integer, Integer> map = new HashMap<>();
+        int left_pointer = 0;
+        int right_pointer = nums.length - 1;
 
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
-        }
+        while (left_pointer < right_pointer) {
 
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(target - nums[i]) && (map.get(target - nums[i]) != i)) {
-                return new int[]{i, map.get(target - nums[i])};
+            if (nums[left_pointer] + nums[right_pointer] == target) {
+                int out1 = -1;
+                int out2 = -1;
+
+                for (int i = 0; i < nums.length; i++) {
+
+                    if (origin[i] == nums[left_pointer] && out1 == -1) {
+                        out1 = i;
+                        continue;
+                    }
+
+                    if (origin[i] == nums[right_pointer] && out2 == -1) {
+                        out2 = i;
+                    }
+                }
+                return new int[]{out1, out2};
+
+            } else if (nums[left_pointer] + nums[right_pointer] < target) {
+                left_pointer++;
+
+            } else if (nums[left_pointer] + nums[right_pointer] > target) {
+                right_pointer--;
             }
         }
 
-        return new int[]{};
+        return new int[0];
+        
+        // // HashMap
+        // Map<Integer, Integer> map = new HashMap<>();
+
+        // for (int i = 0; i < nums.length; i++) {
+        //     map.put(nums[i], i);
+        // }
+
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (map.containsKey(target - nums[i]) && (map.get(target - nums[i]) != i)) {
+        //         return new int[]{i, map.get(target - nums[i])};
+        //     }
+        // }
+
+        // return new int[]{};
     }
 }
