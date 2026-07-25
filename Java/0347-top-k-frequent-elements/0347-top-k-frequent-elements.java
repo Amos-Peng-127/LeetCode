2eeae3e410996619d090e88d4f6661008aa36fb9
Zhixiang Peng <peng0127@bu.edu>
2026-07-25 17:27:57 +0800

    Time: 11 ms (96.01%), Space: 51.7 MB (5.37%) - LeetHub

--- Java/0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ Java/0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,20 +1,51 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        // Hash Map
+        // Bucket Sort
+        List<List<Integer>> freq = new ArrayList<>(nums.length + 1);
+        
+        for (int i = 0; i <= nums.length; i++) {
+            freq.add(new ArrayList<>());
+        }
+
         Map<Integer, Integer> count = new HashMap<>();
 
         for (int n : nums) {
             count.put(n, count.getOrDefault(n, 0) + 1);
         }
-        // System.out.println(count);
-        ArrayList<int[]> count_List = new ArrayList<>();
 
         count.forEach((key, value) -> {
-            count_List.add(new int[]{key, value});
+            freq.get(value).add(key);
         });
-        // System.out.println(count_List);
-        // count_List.stream().map(Arrays::toString).forEach(System.out::println);
-        count_List.sort((a, b) -> b[1] - a[1]);
-        return count_List.stream().limit(k).mapToInt(arr -> arr[0]).toArray();
+
+        int[] top_k = new int[k];
+        int index = 0;
+
+        for (int i = nums.length; i > -1 && index < k; i--) {
+            for (int n : freq.get(i)) {
+                top_k[index] = n;
+                index++;
+                if (index == k) {
+                    break; 
+                }
+            }
+        }
+        return top_k;
+
+        // // Hash Map
+        // Map<Integer, Integer> count = new HashMap<>();
+
+        // for (int n : nums) {
+        //     count.put(n, count.getOrDefault(n, 0) + 1);
+        // }
+        // // System.out.println(count);
+        // ArrayList<int[]> count_List = new ArrayList<>();
+
+        // count.forEach((key, value) -> {
+        //     count_List.add(new int[]{key, value});
+        // });
+        // // System.out.println(count_List);
+        // // count_List.stream().map(Arrays::toString).forEach(System.out::println);
+        // count_List.sort((a, b) -> b[1] - a[1]);
+        // return count_List.stream().limit(k).mapToInt(arr -> arr[0]).toArray();
     }
 }
